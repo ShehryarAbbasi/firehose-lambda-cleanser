58d17f589a293bfd5513e39a027cae310e88785f
Shehryar Abbasi <shehryar.abbasi@gmail.com>
2020-12-15 21:59:03 -0600

    Update properties-to-delete.js

--- src/properties-to-delete.js
+++ src/properties-to-delete.js
@@ -1,5 +1,5 @@
 // Note: 
-// Below is list of Keys which may contain PII Values
+// Below is list of Keys which may contain unwanted or unnecessary data
 // These Keys are to be deleted regardless of Nesting Depth (Parent, Child) or Type (Array, Object)
 
 module.exports = [
